36a7ec4a747f65a20c1dd3fbfc1706c25b66b5d4
Vibhas Jayvant Natekar <natekarvibhas@gmail.com>
2025-11-26 22:22:00 +0530

    being first step to portfolio development by developing navbar and give responsiness.

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import './App.css'
+import Navbar from './components/navbar'
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
+import Projects from './components/Projects'
+import About from './components/About'
+import Resume from './components/Resume'
+
+function App() {
+
+  return (
+    <>
+
+  
+
+      <BrowserRouter>
+         <Navbar/>
+        <Routes>
+          <Route path="projects" element={<Projects/>} />
+          <Route path="about" element={<About/>} />
+          <Route path="resume" element={<Resume/>} />
+
+
+        </Routes>
+      </BrowserRouter>
+
+
+    </>
+  );
+
+
+}
+
+export default App
---
+++ src/components/navbar.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+import About from './About'
+import Resume from './Resume'
+import { DiGitBranch, DiGithubBadge } from 'react-icons/di'
+import { GiThreeBurningBalls, GiThreeKeys, GiTreeBranch } from 'react-icons/gi'
+import { IoGitBranchSharp } from 'react-icons/io5'
+import './navbar.css'
+import { GoHome, GoProject, GoProjectTemplate } from 'react-icons/go'
+import { HiHome } from 'react-icons/hi'
+import { BiUser } from 'react-icons/bi'
+import { GrNotes } from 'react-icons/gr'
+import { FaCross, FaHamburger, FaHammer } from 'react-icons/fa'
+import { MdCancel, MdMenu } from 'react-icons/md'
+import { CgCross } from 'react-icons/cg'
+
+const Navbar = () => {
+  const [ismenuopen, setismenuopen] = useState(false)
+  return (
+    <>
+      <div className='nav-container' >
+        <h2 className='logo' >Vn.</h2>
+    display: none;
+   
+        <h2 onClick={()=>setismenuopen(true)} ><MdMenu/></h2>
+        <ul  className={ismenuopen ? 'side-bar ':'nav-items'} >
+          
+        {ismenuopen && (
+  <h2 className='cancel-icon' onClick={() => setismenuopen(false)}>
+    <MdCancel />
+  </h2>
+)}
+          <NavLink to={'/'}><li> <GoHome/> Home</li></NavLink>
+          <NavLink to={'/projects'}><li> <GoProject/> Project</li></NavLink>
+          <NavLink to={'/about'}><li > <BiUser/> About me</li></NavLink>
+            <NavLink to={'/about'}><li > <GrNotes/> Resume</li></NavLink>
+          <NavLink to={'#'}> <li className='git-btn' >  <IoGitBranchSharp/> </li> </NavLink>
+
+
+
+        </ul>
+      </div>
+
+
+    </>
+  )
+}
+
+export default Navbar
