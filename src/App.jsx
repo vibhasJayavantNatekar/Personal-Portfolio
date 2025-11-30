de03f2176038cbaccea2075ca0812835e494d438
Vibhas Jayvant Natekar <natekarvibhas@gmail.com>
2025-11-30 12:14:46 +0530

    Some small errors

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './components/navbar'
 import Projects from './components/Projects'
 import About from './components/About'
 import Resume from './components/Resume'
-import Herosection from './components/herosection'
+import Herosection from './components/Herosection'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
 
