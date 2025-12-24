22e994b69bde5d8c90314a223e15836cff4888df
Vibhas Jayvant Natekar <natekarvibhas@gmail.com>
2025-12-24 18:19:50 +0530

    Solve silly mistakes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
         <div className="about-container">
           <div className="my-self">
             <h1 className='section-heading'>Know Who <span className='name'>I'M </span> </h1>
-            <p>Hi everyone! I'm <span className="name">Vibhas Natekar </span> from Maharastra , India   <br /> I'm currently pursuing <span className="name" > BCA </span> at <span className="name" >Kavi Kulguru Kalidas  Sanskrit Vishwvidylay, Ramtek </span> <br /> I build full-stack applications using <span className="name"> MongoDB , Express , React , and Node.js </span>,and I love Creating modern UIs with powerful backend logic.   </p>
+            <p>Hi everyone! I'm <span className="name">Vibhas Natekar </span> from Maharastra , India   <br /> I'm currently pursuing <span className="name" > BCA </span> at <span className="name" >Kavi Kulguru Kalidas  Sanskrit Vishwvidyalay, Ramtek </span> <br /> I build full-stack applications using <span className="name"> MongoDB , Express , React , and Node.js </span>,and I love Creating modern UIs with powerful backend logic.   </p>
           </div>
           <img src={ProfileImg} alt="" className='profile-avatar' />
         </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,9 @@
 
 import './Footer.css'
 import { BiLogoGithub } from 'react-icons/bi'
+import { CgCopyright } from 'react-icons/cg'
+import { FaCopyright } from 'react-icons/fa'
+import { LuCopyright } from 'react-icons/lu'
 import { RxLinkedinLogo } from 'react-icons/rx'
 
 const Footer = () => {
@@ -12,10 +15,11 @@ const Footer = () => {
     <div className="footer-wrapper">
       <div className="footer-container">
         <p >Designed and Developed by Vibhas Natekar</p>
-        <p className='copy'>Copyright @ 2025 Vn</p>
+        <p className='copy'>Copyright   <LuCopyright/>  2025 Vn</p>
         <div className="social_icons">
           <h2> <a href="https://github.com/vibhasJayavantNatekar/Inventory-App.git"><BiLogoGithub/></a> <a href="https://www.linkedin.com/in/vibhas-natekar-75a410232?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"> <RxLinkedinLogo/> </a> </h2>
         </div>
+      
       </div>
     </div>
 
